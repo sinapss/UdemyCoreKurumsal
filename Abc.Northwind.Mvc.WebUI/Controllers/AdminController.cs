8d7ad960cdee7e9c4ec87ddaa61259a9509e6dd2
Oguz Cenal <44159361+sinapss@users.noreply.github.com>
2023-02-20 02:17:43 +0300

    Sepet Listelenmesi ve Model validation

---
+++ Abc.Northwind.Mvc.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Mvc.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abc.Northwind.Mvc.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductService _productService;
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var productListViewModel = new ProductListViewModel
+            {
+                Products = _productService.GetAll()
+            };
+            return View(productListViewModel);
+        }
+    }
+}
--- Abc.Northwind.Mvc.WebUI/Controllers/CartController.cs
+++ Abc.Northwind.Mvc.WebUI/Controllers/CartController.cs
@@ -1,4 +1,6 @@
 ﻿using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Entities.Concrete;
+using Abc.Northwind.Mvc.WebUI.Models;
 using Abc.Northwind.Mvc.WebUI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,6 +26,45 @@ namespace Abc.Northwind.Mvc.WebUI.Controllers
             _sessionService.SetCart(cart);
             TempData.Add("message", String.Format("Your product, {0}, was successfully added to the cart!", productToBeAdded.ProductName));
             return RedirectToAction("Index", "Product");
+        }
+
+        public ActionResult List()
+        {
+            var cart = _sessionService.GetCart();
+            CartSummaryViewModel cartListViewModel = new CartSummaryViewModel
+            {
+                Cart = cart
+            };
+            return View(cartListViewModel);
+        }
+
+        public ActionResult Remove(int productId) 
+        {
+            var cart = _sessionService.GetCart();
+            _cartService.RemoveFromCart(cart, productId);
+            _sessionService.SetCart(cart);
+            TempData.Add("message", "Your product was successfully removed from the cart!");
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Complete()
+        {
+            var shippingDetailsViewModel = new ShippingDetailsViewModel
+            {
+                ShippingDetails = new ShippingDetails()
+            };
+            return View(shippingDetailsViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Complete(ShippingDetails shippingDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            TempData.Add("message",String.Format("Thank you {0}, you order is in process", shippingDetails.FirstName));
+            return View();
         }
     }
 }
